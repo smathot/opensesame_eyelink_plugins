24d8f5ca9ff638b654453511349c1762a402d03a
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-04-03 12:24:15 +0200

    Update README and bump to 0.16

--- eyelink_calibrate/eyelink_calibrate.py
+++ eyelink_calibrate/eyelink_calibrate.py
@@ -35,7 +35,7 @@ class eyelink_calibrate(item.item):
 		Constructor
 		"""
 
-		self.version = 0.15
+		self.version = 0.16
 
 		# The item_typeshould match the name of the module
 		self.item_type = "eyelink_calibrate"
