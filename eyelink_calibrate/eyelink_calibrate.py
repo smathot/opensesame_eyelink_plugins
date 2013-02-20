caead5ac55f7d22e4e8f517ea4a0400b4c8dca67
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-02-20 14:04:47 +0100

    Bumpt to 0.23

--- eyelink_calibrate/eyelink_calibrate.py
+++ eyelink_calibrate/eyelink_calibrate.py
@@ -35,7 +35,7 @@ class eyelink_calibrate(item.item):
 		Constructor
 		"""
 
-		self.version = 0.22
+		self.version = 0.23
 
 		# The item_typeshould match the name of the module
 		self.item_type = "eyelink_calibrate"
